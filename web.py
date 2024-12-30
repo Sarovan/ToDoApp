9134a11183aa95a5e7bfe6d1f5533ed9c0aaf625
Sarovan <44712196+Sarovan@users.noreply.github.com>
2024-12-30 20:37:36 +0500

    Implement new to items feature

--- web.py
+++ web.py
@@ -1,12 +1,23 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo + "\n")
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
 st.title("My Todo App")
 st.subheader("Developed to learn Python")
 st.write("It's my first web app")
 
-todos = functions.get_todos()
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter a todo")
+st.text_input(label="", placeholder="Enter a todo", on_change=add_todo, key="new_todo")
+
+st.session_state
